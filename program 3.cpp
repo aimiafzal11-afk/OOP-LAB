6ad5d28c64a84f3d4992332a952d96a19dc927d9
Aiman Afzal <aimiafzal.11@gmail.com>
2026-03-24 22:15:41 +0500

    Payroll.cpp

--- program 3.cpp
+++ program 3.cpp
@@ -1,3 +1,8 @@
+/* Design a Payroll class that has data members for an employee’s hourly pay rate, number of hours worked, and total pay for the week. 
+Write a program with an array of seven Payroll objects. The program should ask the user for the number of hours each employee has worked 
+and will then display the amount of gross pay each has earned. 
+Input Validation: Do not accept values greater than 60 for the number of hours worked. */
+
 #include<iostream>
 using namespace std;
 class Payroll {
