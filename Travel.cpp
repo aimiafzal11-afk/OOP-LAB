3a7ed0fddc070d1153b21a55820f45066380befb
Aiman Afzal <aimiafzal.11@gmail.com>
2026-03-30 22:15:45 +0500

    Update Travel.cpp

--- Travel.cpp
+++ Travel.cpp
@@ -1,3 +1,9 @@
+/* The objective of this exercise is to understand the declaration of a class with its data members and member functions. Also, the concept of passing objects as 
+function parameters. 
+Create a class Travel with two data members kilometer and hour, class member functions input, show and add, which accept an object of Travel as parameter. It adds 
+the values of data members of the parameter object and the value of the calling object’s data members and displays the result. 
+Declare two objects t1 and t2 of class Travel and input data in both objects. Call the add function with t1 object and pass t2 object as an argument. */
+
 #include <iostream>
 using namespace std;
 
